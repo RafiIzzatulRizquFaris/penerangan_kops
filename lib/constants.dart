ed2f9ece0177907e2089cf10b88237e0631168a4
quifar <risqufaris@gmail.com>
2020-10-22 17:20:39 +0700

    change all variable to static const, add PreferenceKey and LoginResponse

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
 class AppColor{
-  final Color redColor = Color(0xfffd7062);
-  final Color primaryColor = Color(0xffeff8e8);
-  final Color blackColor = Color(0xff172c33);
-  final Color accentColor = Color(0xff689575);
+  static const Color redColor = Color(0xfffd7062);
+  static const Color primaryColor = Color(0xffeff8e8);
+  static const Color blackColor = Color(0xff172c33);
+  static const Color accentColor = Color(0xff689575);
+}
+
+class PreferenceKey{
+  static const String id = "id";
+  static const String name = "name";
+  static const String password = "password";
+}
+
+class LoginResponse{
+  static const String WRONG_PASSWORD = "wrong_password";
+  static const String FAILED = "failed";
+  static const String SUCCESS = "success";
 }
