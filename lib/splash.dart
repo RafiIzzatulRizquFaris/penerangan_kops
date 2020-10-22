6fc454ad6d27552d83d47a35f68c3e2b90a962ab
quifar <risqufaris@gmail.com>
2020-10-22 11:43:01 +0700

    add splash.dart and initialize project in firebase

---
+++ lib/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return SplashScreen();
+  }
+
+}
+
+class SplashScreen extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
