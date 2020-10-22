da5164d914371b1f3b869543d80f39a01023fa01
quifar <risqufaris@gmail.com>
2020-10-22 11:53:57 +0700

    add constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  final Color redColor = Color(0xfffd7062);
+  final Color primaryColor = Color(0xffeff8e8);
+  final Color blackColor = Color(0xff172c33);
+  final Color accentColor = Color(0xff689575);
+}
