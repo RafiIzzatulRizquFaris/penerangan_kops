65864b86e1d0369b4e545116fed62fc2e4842a9e
quifar <risqufaris@gmail.com>
2020-10-23 16:30:33 +0700

    add implementation for listing presentation data

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:penerangan_kops/constants.dart';
+import 'package:penerangan_kops/contract/absensi_contract.dart';
+import 'package:penerangan_kops/presenter/absensi_presenter.dart';
 import '../constants.dart';
 
 class Home extends StatefulWidget {
@@ -7,18 +10,39 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> implements AbsensiContractView {
+  AbsensiPresenter absensiPresenter;
+  List<DocumentSnapshot> listAbsensi = List<DocumentSnapshot>();
+  Environment env = Environment();
+  var isLoadData;
+
+  _HomeState() {
+    absensiPresenter = AbsensiPresenter(this);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isLoadData = true;
+    absensiPresenter.loadAbsensiData(env.getDateNow());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Container(
-              height: 160,
-              width: MediaQuery.of(context).size.width,
-              color: AppColor.accentColor,
+            height: 160,
+            width: MediaQuery.of(context).size.width,
+            color: AppColor.accentColor,
+            child: SafeArea(
               child: Padding(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 10,
+                  bottom: 10,
+                ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -26,12 +50,19 @@ class _HomeState extends State<Home> {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         IconButton(
-                            icon: Icon(
-                              Icons.refresh,
-                              size: 24.0,
-                              color: Colors.white,
-                            ),
-                            onPressed: null),
+                          icon: Icon(
+                            Icons.refresh,
+                            size: 24.0,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              isLoadData = true;
+                            });
+                            absensiPresenter.loadAbsensiData(env.getDateNow());
+                          },
+                          splashColor: Colors.lightGreenAccent,
+                        ),
                       ],
                     ),
                     Text(
@@ -53,79 +84,101 @@ class _HomeState extends State<Home> {
                     )
                   ],
                 ),
-              )),
+              ),
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+              child: Text(
+                "20 Oktober 2020",
+                style: TextStyle(
+                    color: AppColor.redColor, fontWeight: FontWeight.w400),
+              ),
+            ),
+          ),
           Expanded(
-              child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
-                child: Text(
-                  "20 Oktober 2020",
-                  style: TextStyle(
-                      color: AppColor.redColor, fontWeight: FontWeight.w400),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.person,
-                    color: AppColor.blackColor,
-                    size: 40,
+            child: isLoadData
+                ? Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: AppColor.accentColor,
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: listAbsensi.length,
+                    itemBuilder: builderAbsensi,
                   ),
-                  title: Text(
-                    "Kelelawar",
-                    style: TextStyle(
-                        color: AppColor.blackColor,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18.0),
-                  ),
-                  trailing: Text(
-                    "09:10",
-                    style:
-                        TextStyle(fontSize: 20.0, color: AppColor.blackColor),
-                  ),
-                  subtitle: Text(
-                    "990 M",
-                    style: TextStyle(color: AppColor.blackColor),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.person,
-                    color: AppColor.blackColor,
-                    size: 40,
-                  ),
-                  title: Text(
-                    "Kelelawar",
-                    style: TextStyle(
-                        color: AppColor.blackColor,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18.0),
-                  ),
-                  trailing: Text(
-                    "09:10",
-                    style:
-                        TextStyle(fontSize: 20.0, color: AppColor.blackColor),
-                  ),
-                  subtitle: Text(
-                    "990 M",
-                    style: TextStyle(color: AppColor.blackColor),
-                  ),
-                ),
-              ),
-            ],
-          )),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        backgroundColor: AppColor.redColor,
-      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        backgroundColor: AppColor.redColor,
+        splashColor: AppColor.accentColor,
+        label: Text(
+          "Absen Sekarang",
+          style: TextStyle(
+            color: AppColor.primaryColor,
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  onErrorAbsen(error) {
+    // TODO: implement onErrorAbsen
+    throw UnimplementedError();
+  }
+
+  @override
+  onSuccessAbsen(String status) {
+    // TODO: implement onSuccessAbsen
+    throw UnimplementedError();
+  }
+
+  @override
+  setAbsensiData(List<DocumentSnapshot> value) {
+    setState(() {
+      listAbsensi = value;
+      isLoadData = false;
+    });
+  }
+
+  @override
+  setOnErrorAbsensi(error) {
+    // TODO: implement setOnErrorAbsensi
+    throw UnimplementedError();
+  }
+
+  Widget builderAbsensi(BuildContext context, int index) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: ListTile(
+        leading: Icon(
+          Icons.person,
+          color: AppColor.blackColor,
+          size: 40,
+        ),
+        title: Text(
+          listAbsensi[index].data["name"],
+          style: TextStyle(
+              color: AppColor.blackColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 18.0),
+        ),
+        trailing: Text(
+          listAbsensi[index].data["time"],
+          style: TextStyle(fontSize: 20.0, color: AppColor.blackColor),
+        ),
+        subtitle: Text(
+          "${listAbsensi[index].data["range"]} M",
+          style: TextStyle(color: AppColor.blackColor),
+        ),
+      ),
     );
   }
 }
