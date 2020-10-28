68fd90de87846db11e2f29f434e0645153c61b9c
quifar <risqufaris@gmail.com>
2020-10-28 21:06:36 +0700

    create user_data_contract.dart

---
+++ lib/contract/user_data_contract.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserDataContractView{
+  onSuccessUserData(List<DocumentSnapshot> value) {}
+  onErrorUserData(error){}
+}
+
+class UserDataContractPresenter{
+  getUserData (){}
+  loadUserData (){}
+}
